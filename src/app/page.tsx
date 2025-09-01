704fbc50b1e50b05c8e8e51f37e806a2d44c9c41
Kartikeya Parihar <kartikvips@gmail.com>
2025-08-31 20:29:33 -0700

    make main page better for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <main 
-      className="min-h-screen flex items-center justify-center p-4 sm:p-8 relative"
+      className="h-screen flex items-center justify-center p-4 sm:p-8 relative"
       style={{
         backgroundColor: `var(--${currentMonth.cssName}-bg)`,
         color: `var(--${currentMonth.cssName}-fg)`
